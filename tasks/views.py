82a12e1955ba4ca31377dd24bc031bff77f8634a
Md. Abdul Mohaimin Al Sayed <mohaimin112233@gmail.com>
2025-06-21 03:48:19 +0600

    Project initialization done

---
+++ tasks/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+    # work with db
+    # transform data
+    # data pass
+    # Http respose / Json response
+    return HttpResponse("Welcome to the task management system")
+
+def contact(request):
+    return HttpResponse("<h1 style='color:red'>This is contact page</h1>")
+
+
+def show_task(request):
+    return HttpResponse("This is the task page")
